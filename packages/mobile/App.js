6280696679b8d46d4a4157b648a1fd20d0c3c3ce
Renan Pupin <renan.pupin@gmail.com>
2020-05-06 22:43:01 -0300

    fix config for monorepo

--- packages/mobile/App.js
+++ packages/mobile/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {StyleSheet, Text, View, Button, TextInput} from 'react-native';
+import {StyleSheet, Text, View, Button, TextInput, TouchableOpacity} from 'react-native';
 import axios from 'axios';
 
 import commonConfig from './src/utils/config';
@@ -61,6 +61,10 @@ class App extends Component {
 
     createTodo = async () => {
         try {
+            if(this.state.creating){
+                return;
+            }
+
             this.setState({
                 creating: true
             });
@@ -83,6 +87,7 @@ class App extends Component {
 
             this.setState({
                 creating: false,
+                text: '',
                 todos: [
                     ...this.state.todos,
                     response.data.todo
@@ -186,53 +191,83 @@ class App extends Component {
                         dispaly: 'flex',
                         flexDirection: 'row',
                         borderColor: "#e1e1e1",
-                        borderWidth: index === this.state.todos.length - 1 ? 0 : 1
+                        // borderWidth: 1,
+                        borderBottomWidth: index === this.state.todos.length-1 ? 0 : 1,
+                        padding: 10,
+                        borderRadius: 3
                     }}
                 >
                     <View
-                        contentEditable={true}
-                        suppressContentEditableWarning={true}
+                        style={{flex: 1, marginRight: 10}}
                         onBlur={(event) => {
                             this.updateTodo(item._id, {text: event.currentTarget.textContent})
                         }}
                     >
                         <Text>{item.text}</Text>
                     </View>
-                    <Button
-                        onPress={() => this.removeTodo(item._id)}
-                        style={{marginLeft: 10}}
-                        title={"Remove"}
-                    />
+                    <View
+                        style={{flex: 0, alignItems: 'flex-end', justifyContent: 'center'}}
+                    >
+                        <TouchableOpacity
+                            onPress={() => this.removeTodo(item._id)}
+                            style={{marginLeft: 10, fontSize: 12}}
+                        >
+                            <Text style={{color: 'blue'}}>Remove</Text>
+                        </TouchableOpacity>
+                    </View>
                 </View>
             )
         });
 
         return (
             <View style={styles.container}>
-                <View style={{flex: 1}}>
+                <View style={{flex: 0, marginBottom: 20}}>
                     <Text style={{fontWeight: "bold"}}>TODOs</Text>
                 </View>
 
                 <View style={{flex: 1}}>
                     {this.state.loading && <Text>Loading...</Text>}
 
-                    <View testID="list-todos">
+                    <View
+                        testID="list-todos"
+                        style={{
+                            borderColor: "#e1e1e1",
+                            borderWidth: 1,
+                            borderRadius: 6,
+                        }}
+                    >
                         {rows}
                     </View>
 
                     {!this.state.loading && rows.length === 0 && <Text>No TODOs found.</Text>}
 
-                    <View style={{flex: 1}}>
+                    <View style={{flex: 1, marginTop: 20}}>
                         <TextInput
                             type={"text"}
                             placeholder={"Write your new TODO"}
                             value={this.state.text}
-                            onChange={(text) => {
-                                this.setState({text: text})
+                            style={{
+                                minWidth: 200,
+                                borderWidth: 1,
+                                borderColor: '#e1e1e1',
+                                paddingHorizontal: 20,
+                                paddingVertical: 10,
+                                borderRadius: 20
+                            }}
+                            disabled={this.state.creating}
+                            onChangeText={(text) => {
+                                this.setState({
+                                    text
+                                });
                             }}
                         />
 
-                        <Button type={"submit"} className={"add-button"} title={"Add"} onPress={this.createTodo}/>
+                        <TouchableOpacity
+                            onPress={this.createTodo}
+                            style={{marginTop: 10, fontSize: 12, alignItems: 'center'}}
+                        >
+                            <Text style={{color: 'blue'}}>Add</Text>
+                        </TouchableOpacity>
                     </View>
                 </View>
             </View>
